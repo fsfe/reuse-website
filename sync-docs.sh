06374692ca3377ec15fca85b39eca447b8d11841
Hans-Christoph Steiner <hans@eds.org>
2020-04-27 22:48:21 +0200

    make site localizable using po4a
    * https://github.com/fsfe/reuse-website/pull/29
    * https://github.com/fsfe/reuse-docs/pull/61

--- sync-docs.sh
+++ sync-docs.sh
@@ -6,9 +6,20 @@
 # This script synchronises all markdown directories from the reuse-docs
 # submodule into the site directory.
 
-cd $(dirname $0)
+basedir=$(cd `dirname $0`; pwd)
+
+cd $basedir/reuse-docs
+po4a po/po4a.conf
 
 for f in faq.md spec.md tutorial.md; do
-  rm -f site/content/"$f"
-  cp reuse-docs/"$f" site/content/"$f"
+  rm -f $basedir/site/content/"$f"
+  cp $basedir/reuse-docs/"$f" $basedir/site/content/"$f"
+  for tf in $basedir/reuse-docs/*/"$f"; do
+      test -e $tf || continue
+      locale=$(basename $(dirname $tf))
+      localedir=$basedir/site/content/$locale
+      test -d $localedir || mkdir $localedir
+      rm -f $basedir/site/content/$locale/"$tf"
+      cp "$tf" $basedir/site/content/$locale/"$f"
+  done
 done
