9ac82dd534756f07ad45f41ef68e7e3ff7545a1e
mxmehl <max.mehl@fsfe.org>
2020-10-23 16:51:47 +0200

    add comments and additional copyright holder

--- sync-docs.sh
+++ sync-docs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Free Software Foundation Europe e.V.
+# SPDX-FileCopyrightText: 2020 Free Software Foundation Europe e.V.
+# SPDX-FileCopyrightText: 2020 Hans-Christoph Steiner <hans@eds.org>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
@@ -11,15 +12,22 @@ basedir=$(cd `dirname $0`; pwd)
 cd $basedir/reuse-docs
 po4a po/po4a.conf
 
+# Only select relevant content files from reuse-docs
 for f in faq.md spec.md tutorial.md; do
+  # remove potentially existing files with the same name
   rm -f $basedir/site/content/"$f"
+  # copy EN base files
   cp $basedir/reuse-docs/"$f" $basedir/site/content/"$f"
+  # check for translated files in subfolders
   for tf in $basedir/reuse-docs/*/"$f"; do
       test -e $tf || continue
+      # locale is the name of the directory (e.g. 'es')
       locale=$(basename $(dirname $tf))
       localedir=$basedir/site/content/$locale
+      # create locale dir on website, and clean it
       test -d $localedir || mkdir $localedir
       rm -f $basedir/site/content/$locale/"$tf"
+      # copy translated files to locale dir
       cp "$tf" $basedir/site/content/$locale/"$f"
   done
 done
